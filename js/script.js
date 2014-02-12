3f002eb1023ee124ebcef6de001b04d92ade0e15
Afnan Fahim <afahim@qatar.cmu.edu>
2014-02-12 02:19:04 -0500

    screen pan in works on multiple devices

--- js/script.js
+++ js/script.js
@@ -6,13 +6,24 @@
   }, false);
 };*/
 
-var init = function() {
-  var card = document.getElementById('fence-container');
-  
-  document.getElementById('fence-container').addEventListener( 'click', function(){
-    card.className = card.className + " lateral-move";
-  }, false);
+$( document ).ready(function() {
+	$("#right-card").height($(window).width());
+	$("#right-card").width($(window).height());
+
+	$("#left-card").height($(window).width());
+	$("#left-card").width($(window).height());
+	
+	var card = document.getElementById('fence-container');
+
+	document.getElementById('fence-container').addEventListener( 'click', function(){
+		card.className = card.className + " lateral-move";
+	}, false);
+});
+
+window.onresize = function(event) {
+	$("#right-card").height($(window).width());
+	$("#right-card").width($(window).height());
+
+	$("#left-card").height($(window).width());
+	$("#left-card").width($(window).height());
 };
-
-
-window.addEventListener('DOMContentLoaded', init, false);
