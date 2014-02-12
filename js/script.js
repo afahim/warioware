c3fdfb419cc5f641fe3a56a5fe6be56b13b40ab9
Afnan Fahim <afahim@qatar.cmu.edu>
2014-02-12 01:29:03 -0500

    implemented css 3d pan out

--- js/script.js
+++ js/script.js
@@ -1,9 +1,18 @@
-var init = function() {
+/*var init = function() {
   var card = document.getElementById('card');
   
   document.getElementById('fence').addEventListener( 'click', function(){
-    card.toggleClassName('flipped');
+    card.className = card.className + " lateral-move";
+  }, false);
+};*/
+
+var init = function() {
+  var card = document.getElementById('fence-container');
+  
+  document.getElementById('fence-container').addEventListener( 'click', function(){
+    card.className = card.className + " lateral-move";
   }, false);
 };
+
 
 window.addEventListener('DOMContentLoaded', init, false);
