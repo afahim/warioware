77134ac4c979e64ad60adaa776f49e9a1f71801d
Afnan Fahim <afahim@qatar.cmu.edu>
2014-02-20 14:39:26 -0500

    complete responsive game experience with atv game and question screen

--- js/script.js
+++ js/script.js
@@ -1,102 +1,103 @@
 var nGames = 2;
 var fenceContainer;
 
-$( document ).ready(function() {
-	fenceContainer = document.getElementById('fence-container');
+//Functions
+var invertWidthHeight, setupGameplayHandlers, nextGame, startGame1,
+  askQuestion, startGame2;
 
-	//inverting 
-	invertWidthHeight();
-	setupGameplayHandlers();
+$(document).ready(function () {
+  fenceContainer = document.getElementById('fence-container');
 
-	//Choosing random game from list of choices and displaying to user
-	nextGame();
+  //inverting 
+  invertWidthHeight();
+  setupGameplayHandlers();
+
+  //Choosing random game from list of choices and displaying to user
+  nextGame();
 });
 
 function nextGame() {
-	var gameIndex = Math.floor(Math.random() * nGames);
-	$("#game-instance").html( $("#game" + gameIndex).html() );
-	if (gameIndex === 0) {
-		startGame1();
-	}
-	else if (gameIndex == 1) { //if a question was called
-		askQuestion();
-	}
-	else if (gameIndex == 2) { //if a question was called
-		startGame2();
-	}
+  var gameIndex = Math.floor(Math.random() * nGames);
+  $("#game-instance").html($("#game" + gameIndex).html());
+  if (gameIndex === 0) {
+    startGame1();
+  } else if (gameIndex === 1) { //if a question was called
+    askQuestion();
+  } else if (gameIndex === 2) { //if a question was called
+    startGame2();
+  }
 }
 
 function gameFinished() {
-	//$("#fence-container").show();
-	$("#fence-container").css('visibility', '');
-	fenceContainer.className = "lateral-back";
+  //$("#fence-container").show();
+  $("#fence-container").css('visibility', '');
+  fenceContainer.className = "lateral-back";
 }
 
-$( window ).resize(function() {
-	invertWidthHeight();
+$(window).resize(function() {
+    invertWidthHeight();
 });
 
 function invertWidthHeight () {
-	$("#right-card").height($(window).width());
-	$("#right-card").width($(window).height());
+    $("#right-card").height($(window).width());
+    $("#right-card").width($(window).height());
 
-	$("#left-card").height($(window).width());
-	$("#left-card").width($(window).height());
+    $("#left-card").height($(window).width());
+    $("#left-card").width($(window).height());
 }
 
 function setupGameplayHandlers() {
-	var card = document.getElementById('card');
+    var card = document.getElementById('card');
 
-	fenceContainer.addEventListener("webkitAnimationEnd", function(e){
-		if(e.animationName === "pan-in") {
-			$("#fence-container").css('visibility', 'hidden');
-		}
-		else if (e.animationName === "knock-over") {
-			fenceContainer.className = "lateral-move";
-		}
-		else if (e.animationName === "pan-out") {
-			$(".door-holder").removeClass("not-slided");
-			$(".door-holder").addClass("slided");
-		}
-		else if (e.animationName === "slidein-left-door") {
-			nextGame();
-			$(".door-holder").removeClass("slided");
-			$(".door-holder").addClass("not-slided");
-		}
-		else if (e.animationName === "slideout-left-door") {
+    fenceContainer.addEventListener("webkitAnimationEnd", function(e){
+      if(e.animationName === "pan-in") {
+          $("#fence-container").css('visibility', 'hidden');
+        }
+        else if (e.animationName === "knock-over") {
+          fenceContainer.className = "lateral-move";
+        }
+        else if (e.animationName === "pan-out") {
+          $(".door-holder").removeClass("not-slided");
+          $(".door-holder").addClass("slided");
+        }
+        else if (e.animationName === "slidein-left-door") {
+          $(".door-holder").removeClass("slided");
+          $(".door-holder").addClass("not-slided");
+        nextGame();
+        }
+        else if (e.animationName === "slideout-left-door") {
+          fenceContainer.className = "lateral-move";
+        }
+    }, false);
 
-			fenceContainer.className = "lateral-move";
-		}
-	}, false);
+    var clicked = false;
 
-	var clicked = false;
+    document.getElementById('right-card').addEventListener('click', function(){
+        if (clicked === false) {
+          fenceContainer.className = "lateral-move";
+          clicked = true;
+        } else {
+          fenceContainer.className = "lateral-back";
+          clicked = false;
+        }
+    }, false);
 
-	document.getElementById('right-card').addEventListener('click', function(){
-		if (clicked === false) {
-			fenceContainer.className = "lateral-move";
-			clicked = true;
-		} else {
-			fenceContainer.className = "lateral-back";
-			clicked = false;
-		}
-	}, false);
+    document.getElementById('card').addEventListener( 'click', function(){
+        card.className = card.className + " flipped";
+    }, false);
 
-	document.getElementById('card').addEventListener( 'click', function(){
-		card.className = card.className + " flipped";
-	}, false);
+    document.getElementById('top-card').addEventListener('click', function(){
+        $(".door-holder").removeClass("not-slided");
+        $(".door-holder").addClass("slided");
+    });
 
-	document.getElementById('top-card').addEventListener('click', function(){
-		$(".door-holder").removeClass("not-slided");
-		$(".door-holder").addClass("slided");
-	});
+    document.getElementById('left-door').addEventListener('click', function(){
+        $(".door-holder").removeClass("slided");
+        $(".door-holder").addClass("not-slided");
+    });
 
-	document.getElementById('left-door').addEventListener('click', function(){
-		$(".door-holder").removeClass("slided");
-		$(".door-holder").addClass("not-slided");
-	});
-
-	document.getElementById('right-door').addEventListener('click', function(){
-		$(".door-holder").removeClass("slided");
-		$(".door-holder").addClass("not-slided");
-	});
+    document.getElementById('right-door').addEventListener('click', function(){
+        $(".door-holder").removeClass("slided");
+        $(".door-holder").addClass("not-slided");
+    });
 }
